a43368ffe6b04257a2bb49bd42da8ee52c95522b
Andrew Korolev <korolev.andrej2004@yandex.ru>
2023-02-23 17:35:05 +0300

    first commit

---
+++ js/map.js
@@ -0,0 +1,62 @@
+let myMap = NaN;
+let geoMarks = {};
+let names = {};
+let ids = NaN;
+ymaps.ready(init);
+const paramsString = ''
+
+var socket = new WebSocket("ws://188.120.227.130:8765");
+
+
+
+socket.onopen = function() {
+    let searchParams = new URLSearchParams(window.location.search)
+    // ids = searchParams.getAll('id[]')
+    ids = [12,245]
+    socket.send(JSON.stringify({'type':'subscribe', 'id':ids}))//todo:тут будут id из url
+};
+
+socket.onmessage = function(event) {
+    console.log(event.data)
+    add_marker_test(JSON.parse(event.data)['id'], JSON.parse(event.data)['coords'])
+};
+
+function init() {
+     myMap = new ymaps.Map("map", {
+            center: [55.76, 37.64],
+            zoom: 10
+        })
+}
+
+function add_marker(id, coords) {
+    myMap.geoObjects.remove(geoMarks[id])
+    geoMarks[id] = new ymaps.GeoObject({
+        geometry: {
+            type: "Point",
+            coordinates: coords,
+            preset: 'islands#redIcon',
+        },
+        properties: {
+            iconContent: names[id],
+        },
+    },
+        {
+            preset: 'islands#blueStretchyIcon',
+        });
+    myMap.geoObjects.add(geoMarks[id]);
+
+}
+
+
+function add_marker_loop(id, coords) {
+    setTimeout(function() {
+        add_marker(id, coords)
+    }, 1000)
+}
+
+
+
+function add_marker_test(id, coords) {
+    names[id] = ids.indexOf(id) + 1;
+    add_marker_loop(id, coords);
+}
